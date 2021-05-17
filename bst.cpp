3d4afb5503f65c5e9db5d55d1fdd900b78d8c11d
Renektonn <59362328+Renektonn@users.noreply.github.com>
2021-05-17 15:05:08 +0800

    Update bst.cpp
    Implement address and search

--- bst.cpp
+++ bst.cpp
@@ -1,59 +1,127 @@
 #include <iostream>
 
+
+
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
 };
 
+static TreeNode * root;
 
-void delete(){
+TreeNode * address(int key){
+	
+	TreeNode * current;
+	current=root;
+	
+	while(current!=NULL and key!=current->val){
+		
+		
+		if(key>current->val){
+			current=current->right;
+		}
+		else
+			if(key<current->val){
+				current=current->left;	
+			}
+	}
+	return current;
+} 
 
+
+bool search(int key){
+	
+	TreeNode * current;
+	current=root;
+	//root定非NULL 而current又等於root 故current非NULL 
+	while(key!=current->val){
+		
+		
+		if(key>current->val){
+			current=current->right;
+		}
+		else
+			if(key<current->val){
+				current=current->left;	
+			}
+		
+		if(current==NULL) return false;
+			
+		
+	}
+	return true;
+	
 }
 
-
-
-void search(){
-
+void modify(int key){
+	if(search(key)){ //如果有此鍵值，才修改 
+		TreeNode * current=address(key);
+		current->val=key;
+	}
 }
+
+void del(int key){
+	if(search(key)){ //如果有此鍵值，才刪除 
+		
+	} 
+}
+
 
 void inorder(){
 
 }
 
-void insert(){
-  //init root
-        TreeNode * root=new TreeNode;
-        cin>>root->val;
-        root->left=NULL;
-        root->right=NULL;
+void insert(int key){
+  	
         
-        //t
-        TreeNode * current;
-        current=root;
-        
-        int temp;
-        cin>>temp;
-        //input is greater than current->val
-        if(temp>current->val){
-          if(current->right!=NULL){
-              current=current->right;
-          }
-          else{
-            current->right=new TreeNode;
+    //t
+    TreeNode * current;
+    current=root;
+    
+    //input is greater than current->val
+    if(key>current->val){
+        if(current->right!=NULL){
             current=current->right;
-            current.val=temp;
-          }
         }
-        //input is less than current->val
-        if(temp<current->val){
-          if(current->left!=NULL){
-              current=current->left;
-          }
-          else{
-            current->left=new TreeNode;
+        else{
+        	current->right=new TreeNode;
+        	current=current->right;
+        	current->val=key;
+        }
+    }
+    //input is less than current->val
+    if(key<current->val){
+        if(current->left!=NULL){
             current=current->left;
-            current.val=temp;
-          }  
         }
+        else{
+        	current->left=new TreeNode;
+        	current=current->left;
+        	current->val=key;
+        }  
+    }
 }
+
+
+int main(){
+	//init root
+	TreeNode * root=new TreeNode;
+	std::cin>>root->val;
+	root->left=NULL;
+	root->right=NULL;
+	
+	insert(2);
+	insert(1);
+	insert(3);
+	
+	
+}
+
+
+
+
+
+
+
+
